4f7850534f7c25b731258bd505d1bfca4909de84
Scott O'Malley <somalley@guidewire.com>
2016-05-11 18:38:37 +0100

    Initial Commit

---
+++ app/main.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', ['breeze.angular']);
+
+angular.module('app').controller('breezeTest', ['$scope', 'breeze', breezeCtrl]);
+
+function breezeCtrl($scope, breeze) {
+
+    breeze.NamingConvention.camelCase.setAsDefault();
+
+    var ds = new breeze.DataService({
+        serviceName: 'http://localhost:9000',
+        hasServerMetadata: false
+    });
+
+    var manager = new breeze.EntityManager({dataService: ds});
+
+    var query = new breeze.EntityQuery()
+        .from("fencer");
+
+    manager.executeQuery(query)
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (e) {
+            console.error(e);
+        });
+
+}
---
+++ main.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fortune = require('fortune');
+const jsonApiSerializer = require('fortune-json-api');
+const nedbAdapter = require('fortune-nedb');
+
+
+const instance = new fortune({
+    fencer: {
+        firstName: { type: String },
+        lastName: { type: String },
+        gender: { type: String },
+        club: { type: String },
+        iffNumber: { type: String },
+        results: {link : 'result', isArray: true, inverse:'fencer'}
+    },
+
+    result: {
+        placing: { type: Number },
+        fencer: {link: 'fencer', isArray: false, inverse: 'results'},
+        weapon: { type: String },
+        points: { type: Number },
+        event: {link: 'event', isArray: false, inverse: 'results'}
+    },
+
+    event: {
+        eventDate: {type: Date},
+        results: {link: 'result', isArray: true, inverse: 'event'},
+        weapon: { type: String },
+        competition: {link: 'competition', isArray: false, inverse: 'events'}
+    },
+
+    competition: {
+        name: { type: String },
+        shortName: { type: String },
+        events: {link: 'event', isArray: true, inverse: 'competition'}
+    }
+});
+
+
+// `instance` is an instance of Fortune.js.
+const listener = fortune.net.http(instance, {
+    //serializers: [
+    //    // The `options` object here is optional.
+    //    [ jsonApiSerializer, {/*options*/} ]
+    //],
+    //adapter: [ nedbAdapter, {/*options*/} ]
+});
+
+// The listener function may be used as a standalone server, or
+// may be composed as part of a framework.
+const server = http.createServer(function(request, response){
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+    return listener(request, response);
+});
+
+server.listen(9000);
+console.log("Listening on port: 9000");
